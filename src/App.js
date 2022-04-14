35612207a0c38e15157117a6358880e313f6e943
Canh Doan <doanc@saic.com>
2022-04-14 14:33:57 -0400

    Parent app with selection example

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
-function App() {
+import CountDown from './components/countdown/App';
+import Selection from './components/selection/Selection';
+
+const App = () => {
   return (
     <div>
-      <h1>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </h1>
+      <CountDown></CountDown>
+      <Selection></Selection>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ParentApp from './App';
 
-// import BasicTable from "./BasicTable";
-// import GroupedTable from "./GroupedTable";
-// import GroupedTableV2 from "./GroupedTableV2";
-// import Sorting from "./Sorting";
-
-import CountDownApp from "./components/countdown/App";
-ReactDOM.render(<CountDownApp />, document.getElementById('root'));
+ReactDOM.render(<ParentApp />, document.getElementById('root'));
